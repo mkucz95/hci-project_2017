12cc4c88c91341837ac1022f8cf141dc6fc5d7ce
Sooyeon Chough <schough@ucsd.edu>
2017-02-08 21:01:23 -0800

    Trying first screen html

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
-
 /*
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(req, res) {
   res.render('index');
 };
+
+exports.view = function( req, res ) {
+	res.render( 'index', {
+		'favors': [
+			{ 'name': 'Sample',
+			'image': 'sample.png',
+			'id': 'favor1'
+			},
+			{ 'name': 'Sample',
+				'image': 'sample.png',
+				'id': 'favor2'
+			}
+		]
+	})
+}
