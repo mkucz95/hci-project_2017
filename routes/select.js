e5ca37dc07de5a01ecdd57af08031bba601e540c
Mateusz Kucz <mati.kucz95@gmail.com>
2017-02-09 23:34:47 -0800

    json not working yet

--- routes/select.js
+++ routes/select.js
@@ -1,7 +1,27 @@
 var data = require('../data.json');
 
-exports.view= function(req, res) { 
-	var name = req.params.name; 
 
-	res.render('select', data);
+exports.viewFavour= function(req, res) { 
+	
+	var id = req.params.id; 
+	console.log(id);
+
+	var name = data.favours[id-1].name;
+	var image = data.favours[id-1].image;
+	var time = data.favours[id-1].time;
+	var location = data.favours[id-1].location;
+	var subject = data.favours[id-1].subject;
+	var description= data.favours[id-1].description;
+
+
+	res.render('select', {
+		'name': name,
+		'image': image,
+		'time': time,
+		'location': location,
+		'subject': subject,
+		'description': description
+	});
+	console.log(subject);
+
 };
