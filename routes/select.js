736b480c6949bfeb26a093725393d6cddd8be0e8
Mateusz Kucz <mati.kucz95@gmail.com>
2017-02-10 23:38:39 -0800

    added bootstrap to handlebars

--- routes/select.js
+++ routes/select.js
@@ -21,20 +21,5 @@ exports.viewFavour= function(req, res) { 
 		'subject': subject,
 		'description': description
 	});
-<<<<<<< HEAD
 
-=======
-	console.log(subject);
-}
-
-exports.view= function(req, res) { 
-	res.render('select', {
-		'image': data.image,
-		'id': data.id,
-		'time': data.time,
-		'location': 'location',
-		'subject': 'data.subject',
-		'description': 'data.description'
-	});
->>>>>>> 208cbf8b0c8f0363b042d4400fd56982d2ec574a
 };
