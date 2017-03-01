2e254310ac14058f6e765c695db8ebf71641d558
Mateusz Kucz <mati.kucz95@gmail.com>
2017-03-01 10:45:03 -0800

    added game functionality

--- routes/profile.js
+++ routes/profile.js
@@ -52,6 +52,48 @@ exports.view = function( req, res ) { 
 	if(requesting.status === ""){
 		requestBool = false; //did not find a requesting favour
 	}
+
+	var level = 1;	//current level
+	var levelUp = 5; //how many to complete to level up
+	var progressBar = "";
+	var progressVal = "0";
+	
+	level = 1 + Math.floor(completed/5);  //whole number
+	levelUp = completed%5; //how many needed to get multiple of 5
+
+
+	if(levelUp == 1){
+ 		progressVal = "80";
+ 		progressBar = "progress-bar-success";
+	}
+
+	if(levelUp == 2){
+		progressVal = "60";
+ 		progressBar = "progress-bar-info";
+
+	}
+
+	if(levelUp == 3){
+		progressVal = "40";
+ 		progressBar = "progress-bar-info";
+
+	}
+
+	if(levelUp == 4){
+		progressVal = "20";
+ 		progressBar = "progress-bar-warning";
+
+	}
+
+	if(levelUp == 5){
+		 progressVal = "5";
+		 progressBar = "progress-bar-danger";
+
+	}
+
+	var totalFavours = inDebt + completed;
+	var owedPercent = inDebt/totalFavours * 100;
+	var completedPercent = completed/totalFavours * 100;
 		
 //define all the variables, not all will be displayed depending on requestBool
 		var name = user.user.name;
@@ -71,6 +113,12 @@ exports.view = function( req, res ) { 
 		'description': description,
 		'inDebt': inDebt,
 		'completed': completed,
-		'requestBool': requestBool
+		'requestBool': requestBool,
+		'level': level,
+		'levelUp': levelUp,
+		'progressBar': progressBar,
+		'progressVal': progressVal,
+		'owedPercent': owedPercent,
+		'completedPercent': completedPercent
 	});
 };
