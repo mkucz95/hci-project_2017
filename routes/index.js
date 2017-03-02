39edf421d467357fd30c4780ef99570130648d88
Mateusz Kucz <mati.kucz95@gmail.com>
2017-03-01 23:24:28 -0800

    homepage sorting works, date doesnt though

--- routes/index.js
+++ routes/index.js
@@ -9,24 +9,27 @@ var user = require('../user.json');
 exports.view = function( req, res ) {
 	//make new variable here to filter
 
-	var timeSort = Array(); //create new empty array to store all non expired favours
+	var timeSort = []; //create new empty array to store all non expired favours
+	var userName = user.user.name; 
+
 
 	for(i=0; i<data.favours.length; i++){
 		var jsonDate = data.favours[i].time;
 		var dateObj = new Date();
 		var currentTime = dateObj.toJSON();
 		var status = data.favours[i].status;
-		if(jsonDate>currentTime && status ==="active"){ //only include the favours that people can select
-			timeSort.push(data.favours[i]);
+
+		if(userName === data.favours[i].name){
+			continue; //skip this request if it is the person's who submited it
+		}
+
+		if(status === "active" ){ //only include the favours that people can select
+			timeSort[timeSort.length]= data.favours[i];
 		}
 	}
 
-
 	var ownRequest = false; //am I requesting something
 	var otherRequest = false; //am I doing something for someone
-
-	var doingFavour = false;
-	var userName = user.user.name; 
 
       for(i=0; i<data.favours.length; i++){
       	if(data.favours[i].name === userName && data.favours[i].status === "inProgress"){
@@ -39,12 +42,10 @@ exports.view = function( req, res ) {
 
       }
 
-	
-
 	//res.render('index', timeSort) //pass in the filtered variable
 	res.render('index', {
 		//'name': name,
-		'favours': data.favours,
+		'favours': timeSort,
 		'ownRequest': ownRequest,
 		'otherRequest': otherRequest
 		}); 
