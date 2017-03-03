520386f62288ac4aac735a7c17451a385e9b0197
Mateusz Kucz <mati.kucz95@gmail.com>
2017-03-02 23:24:57 -0800

    final additions, auto popdown

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -14,38 +14,72 @@ $(document).ready(function() {
 		console.log("url slice: " + urlSlice);
 
 
-	if(urlSlice === "showModal=1"){
+//see which modal was called based on where redirected from
+//open modal
+//automatically close after 3 seconds
+
+	if(urlSlice === "showModal=1"){	
 		console.log("showModal=1");
 		 $('#requestConfirm').modal('show');
+
+		 setTimeout(function(){
+			hideModal('#requestConfirm')
+		}, 5000);
 
 	}
 
 	if(urlSlice ==="showModal=2"){
 		console.log("showModal=2");
 		$('#noConfirmRequest').modal('show');
+
+		setTimeout(function(){
+			hideModal('#noConfirmRequest')
+		}, 5000);
 	}
 
 	if(urlSlice ==="showModal=3"){
 		console.log("showModal=3");
 		$('#completed').modal('show');
+
+		setTimeout(function(){
+			hideModal('#completed')
+		}, 5000);
+		
 	}
 
 	if(urlSlice ==="showModal=4"){
 		console.log("showModal=4");
 		$('#cancelled').modal('show');
+
+		setTimeout(function(){
+			hideModal('#cancelled')
+		}, 5000);
 	}
 
 	if(urlSlice ==="showModal=5"){
 		console.log("showModal=5");
 		$('#accepted').modal('show');
+
+		setTimeout(function(){
+			hideModal('#accepted')
+		}, 5000);
 	}
 
 	if(urlSlice ==="showModal=6"){
 		console.log("showModal=6");
 		$('#withdraw').modal('show');
+
+		setTimeout(function(){
+			hideModal('#withdraw')
+		}, 5000);
 	}
 
   });
+
+function hideModal(e){
+	console.log("hideModal");
+	$(e).click();
+}
 
 function initializePage() {
 	console.log("Javascript connected!");
